693046e65448251146e641e99c66f1f277ca1f7c
zephyrisalive <zephyrisalive@gmail.com>
2013-04-08 08:14:15 +0800

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trans::Application.config.secret_token = '0d8f22b03e84edae3e90027d18add11d325962dbe9335371a466c761de00f2e92d2f06ee2ec9b79e1b4477f5e992d4c1313e31de39a22757c27aaba005cb5a15'
